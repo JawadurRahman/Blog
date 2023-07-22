f090121508edfe3a29a531a7c0d9cd5ac9abf349
JawadurRahman <jawaduratl@gmail.com>
2023-07-21 22:08:17 -0300

    feature add new post, view previous post

--- app.js
+++ app.js
@@ -17,8 +17,9 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Set up a basic route
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const posts = await BlogPost.find({});
+  res.render('index', {posts});
 });
 
 // Start the server
@@ -29,26 +30,66 @@ app.listen(PORT, () => {
 
 // Define the blog post schema
 const blogPostSchema = new mongoose.Schema({
-  title: String,
-  content: String,
+  _id: { type: String, required: true },
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  createdAt: { type: Date, required: true },
 });
 
 // Create the blog post model
 const BlogPost = mongoose.model('BlogPost', blogPostSchema);
 
 // Fetch and display an individual blog post
-app.get('/posts/:id', async (req, res) => {
+app.get("/posts/:id", async function(req, res) {
   const postId = req.params.id;
-
-  try {
-    const post = await BlogPost.findById(postId).exec();
-
-    if (!post) {
-      res.status(404).send('Post not found');
-    } else {
-      res.render('post', { post });
-    }
-  } catch (err) {
-    res.status(500).send('Internal Server Error');
+  const post = await BlogPost.findById(postId);
+  if (!post) {
+    res.status(404).send('<h1>Post not found<h1>');
   }
+  return res.render('post', { post }); //res.status(200).send(post);
 });
+
+app.post('/posts', async (req, res) => {
+  const { newPostId, title, content } = req.body;
+
+  // Create the new blog post object
+  const newBlogPost = {
+    _id: newPostId,
+    title,
+    content,
+    createdAt: new Date(),
+  };
+
+  // Save the new blog post to the database
+  const created = await BlogPost.create(newBlogPost);
+
+  if (created) {
+    res.redirect('/')
+    res.status(201).send('Blog post created successfully');
+  } else {
+    res.status(404).send('<h1>Creating failed not found<h1>');
+  }
+
+});
+
+// Route for rendering the new post creation page
+app.get('/new-post', (req, res) => {
+  res.render('new-post');
+});
+
+
+// app.get('/posts/:id', async (req, res) => {
+//   const postId = req.params.id;
+
+//   try {
+//     const post = await BlogPost.findById(postId).exec();
+
+//     if (!post) {
+//       res.status(404).send('Post not found');
+//     } else {
+//       res.render('post', { post });
+//     }
+//   } catch (err) {
+//     res.status(500).send('Internal Server Error');
+//   }
+// });
